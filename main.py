dc73662868e06d2308dbd36541b12d73d0b814f4
Alankritgupta101 <alankritgupta06@gmail.com>
2024-08-16 18:54:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+computer = random.choice([1,0,-1])
+
+youstr  = input("select your move: ")
+
+youDict = {"stone": -1, "paper": 0,  "scissor": 1}
+you = youDict[youstr]
+reversedict = {-1: "stone", 0: "paper",  1: "scissor"}
+
+print(f"you chose :{reversedict[you]}\n computer chose :{reversedict[computer]}")
+
+if(computer == you):
+    print("the match is draw")
+
+else:
+    if(computer == -1 and you ==0):
+        print("you win!")
+
+    elif(computer == -1 and you ==1):
+        print("you lose!")
+
+    elif(computer == 1 and you ==-1):   
+        print("you win!")
+
+    elif(computer == 1 and you ==0):    
+        print("you lose!")
+
+    elif(computer == 0 and you ==1):
+        print("you win!")
+
+    elif(computer == 0 and you ==-1):
+        print("you lose!")
+
+    else:
+        print("something went wrong")
+        
+
+
+ 
